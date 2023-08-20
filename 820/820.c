f1f326dfb2954bb1d8696ac8fe665319f3fdbbaf
userjack64 <3393450287@qq.com>
2023-08-20 09:49:26 +0800

    添加项目文件。

--- 820/820.c
+++ 820/820.c
@@ -62,8 +62,38 @@
 //	return bank[m];
 //}
 
+//Hanoi problem
+//void move(char x, char y)
+//{
+//	printf("%c-> %c\n",x,y);
+//}
+//void hanoi(int n,char a, char b, char c)
+//{
+//	if (n == 1)
+//	{
+//		move(a, c);
+//	}
+//	else
+//	{
+//		hanoi(n - 1, a, c, b);
+//		move(a, c);
+//		hanoi(n - 1, b, a, c);
+//	}
+//}
+
 int main()
 {
+
+
+
+
+	//int n = 0;
+	//scanf("%d", &n);
+	//hanoi(n, 'a', 'b', 'c');
+
+
+
+
 	//int n;
 	//scanf("%d", &n);
 	//printf("%d", fib(n));
