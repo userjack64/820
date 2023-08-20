85ad5da04cdb4a37d1e686b6754acf12a68f38bd
userjack64 <3393450287@qq.com>
2023-08-20 09:49:26 +0800

    添加项目文件。

--- 820/820.c
+++ 820/820.c
@@ -15,8 +15,61 @@
 //	printf("%d ", n % 10);
 //}
 
+
+//int fib(int m)
+//{
+//	if (m >= 3)
+//	{
+//		return fib(m - 1) + fib(m - 2);
+//	}
+//	else {
+//		return 1;
+//	}
+//}
+
+
+//int Fibno2(int n)
+//{
+//	int num1 = 1;
+//	int num2 = 1;
+//	int tmp = 0;
+//	int i = 0;
+//	if (n < 3)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		for (i = 0; i > n - 3; i++)
+//		{
+//			tmp = num1 + num2;
+//			num1 = num2;
+//			num2 = tmp;
+//		}
+//		return tmp;
+//	}
+//}
+
+
+//int fib(int m)
+//{
+//	int i;
+//	int bank[101] = { 0,1,1 };
+//	for (i = 2; i <= m; i++)   
+//	{
+//		bank[i] = bank[i - 1] + bank[i - 2];   
+//	}
+//	return bank[m];
+//}
+
 int main()
 {
+	//int n;
+	//scanf("%d", &n);
+	//printf("%d", fib(n));
+
+
+
 	//unsigned int num = 0;
 	//scanf("%d", &num);
 	//print01(num);
